122595d57d3c6cbb72e043562cace00cd32f6171
Wesley Chang <weschang15@gmail.com>
2018-10-08 20:36:23 -0700

    Configured react app to listen for socketio events

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from "react";
-import Pusher from "pusher-js";
+import openSocket from "socket.io-client";
 import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   componentDidMount = () => {
-    const pusher = new Pusher("428396da0516ba8fea1f", {
-      cluster: "us2",
-      forceTLS: true
-    });
+    const socket = openSocket("http://localhost:4000");
 
-    const channel = pusher.subscribe("reports");
-    channel.bind("ReportUpdated", function(data) {
-      console.log(data);
+    socket.on("ReportUpdated", report => {
+      console.log(report);
     });
   };
 
