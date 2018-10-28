6ec81694f9ce65c6ef6c7437b374566a3a9e6942
Wesley Chang <weschang15@gmail.com>
2018-10-28 15:36:41 -0700

    Wired up test graphql stuff

--- src/index.js
+++ src/index.js
@@ -1,10 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { render } from "react-dom";
+import ApolloClient, { gql } from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+const client = new ApolloClient({
+  uri: "/graphql"
+});
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const currentUserQuery = gql`
+  query {
+    currentUser {
+      ok
+      errors {
+        path
+        message
+      }
+      user {
+        email
+      }
+    }
+  }
+`;
+
+client.query({ query: currentUserQuery }).then(result => console.log(result));
+
+const logoutMutation = gql`
+  mutation {
+    logoutUser {
+      ok
+      errors {
+        path
+        message
+      }
+    }
+  }
+`;
+
+const logout = e => {
+  client
+    .mutate({ mutation: logoutMutation })
+    .then(result => console.log(result));
+};
+
+const app = (
+  <ApolloProvider client={client}>
+    <button onClick={logout}>Logout</button>
+  </ApolloProvider>
+);
+
+render(app, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
