5a4fd6742cd2df927d3d4b7b576fece9b376e2b1
Wesley Chang <weschang15@gmail.com>
2018-11-18 15:09:01 -0800

    Export sidebar

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,5 +1,6 @@
+export { default as Header } from "./Header/Header";
 export { default as Routes } from "./Routes";
+export { default as Sidebar } from "./Sidebar/Sidebar";
 export { default as Login } from "./Users/Login";
 export { default as Logout } from "./Users/Logout";
 export { default as NewUser } from "./Users/NewUser";
-export { default as Header } from "./Header/Header";
