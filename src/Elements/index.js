b9291e580d9fbdbe3ced18d1fc6bce721a45f61e
Wesley Chang <weschang15@gmail.com>
2018-11-18 15:10:22 -0800

    Export new components

--- src/Elements/index.js
+++ src/Elements/index.js
@@ -1,2 +1,7 @@
 export { default as StackedForm } from "./Forms/StackedForm";
 export { default as StandardInput } from "./Inputs/StandardInput";
+export { default as Icon } from "./Icons/Icon";
+export { default as IconText } from "./Icons/IconText";
+export { default as List } from "./Lists/List";
+export { default as ListItem } from "./Lists/ListItem";
+export { default as Logo } from "./Logo/Logo";
