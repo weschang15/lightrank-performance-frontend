ef88a5cbdc2958d78403018fb159b78fe37026ce
Wesley Chang <weschang15@gmail.com>
2018-11-22 13:11:25 -0800

    Exported project components

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,7 +1,9 @@
 export { default as Dashboard } from "./Dashboard/Dashboard";
 export { default as Header } from "./Header/Header";
+export { default as ProjectsContainer } from "./Projects/ProjectsContainer";
 export { default as Projects } from "./Projects/Projects";
 export { default as ProjectList } from "./Projects/ProjectList";
+export { default as ProjectListItem } from "./Projects/ProjectListItem";
 export { default as NewProject } from "./Projects/NewProject";
 export { default as Routes } from "./Routes";
 export { default as Sidebar } from "./Sidebar/Sidebar";
