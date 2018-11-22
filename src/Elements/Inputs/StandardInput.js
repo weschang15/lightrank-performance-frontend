66532e848bc00d7fbe9bdfba509bed884a57f8f8
Wesley Chang <weschang15@gmail.com>
2018-11-21 20:34:04 -0800

    Updated input to use styled component theme

--- src/Elements/Inputs/StandardInput.js
+++ src/Elements/Inputs/StandardInput.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 
 const StandardInput = styled.input`
   margin: 0.5em 5px;
-  padding: 0.75em 1em;
+  padding: 12px 1em;
   font-size: 16px;
-  border-radius: 2px;
-  border: 1px solid #e8e8e8;
+  border-radius: 6px;
+  border: 1px solid ${({ theme }) => theme.border};
   color: #141414;
 `;
 
