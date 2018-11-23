a60d1ba96a53859aea27e4ed848a0351fbdd74d1
Wesley Chang <weschang15@gmail.com>
2018-11-22 21:16:10 -0800

    Removed dead code

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,35 +1,17 @@
 import React from "react";
-import { Query } from "react-apollo";
-import { gql } from "apollo-boost";
 import { PageContainer } from "Elements";
 import ProjectsContainer from "./ProjectsContainer";
 import NewProject from "./NewProject";
 
 const Projects = () => {
   return (
-    <Query query={GET_AUTH}>
-      {({ data: { auth }, loading }) => {
-        if (loading) return null;
-
-        return (
-          <PageContainer>
-            <h1>Your projects</h1>
-            <NewProject />
-            <h1>All Projects</h1>
-            <ProjectsContainer user={auth.user} />
-          </PageContainer>
-        );
-      }}
-    </Query>
+    <PageContainer>
+      <h1>Your projects</h1>
+      <NewProject />
+      <h1>All Projects</h1>
+      <ProjectsContainer />
+    </PageContainer>
   );
 };
-
-const GET_AUTH = gql`
-  query GetAuth {
-    auth @client {
-      user
-    }
-  }
-`;
 
 export default Projects;
