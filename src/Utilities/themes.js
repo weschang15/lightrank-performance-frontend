f82d8714aaa1fc3673342f7b4b0a9a98c236b39c
Wesley Chang <weschang15@gmail.com>
2018-11-25 17:51:15 -0800

    Added theme colors

--- src/Utilities/themes.js
+++ src/Utilities/themes.js
@@ -1,14 +1,45 @@
 export const primaryTheme = {
+  colors: {
+    white: "#f7f7f7",
+    black: "#161c21",
+    darkest: "#272838",
+    darker: "#3e3e48",
+    dark: "#6b6b73",
+    light: "#c3c3c6",
+    lighter: "#d2d2d4",
+    lightest: "#e0e0e2"
+  },
+  weights: {
+    normal: 400,
+    bold: 700,
+    heavy: 900
+  },
   border: "#dee6f0",
   borderRadius: "3px",
   button: {
-    default: {
-      bg: "#f8b200",
-      color: "#664900"
+    disabled: {
+      bg: "#c3c3c6",
+      color: "#3e3e48"
     },
-    hover: {
-      bg: "#fad26b",
-      color: "#664900"
+    primary: {
+      default: {
+        bg: "#f8b200",
+        color: "#664900"
+      },
+      hover: {
+        bg: "#fad26b",
+        color: "#664900"
+      }
+    },
+    danger: {
+      default: {
+        bg: "#f83600",
+        color: "#3a0303"
+      },
+      hover: {
+        bg: "#fa7b58",
+        color: "#3a0303"
+      }
     }
   }
 };
