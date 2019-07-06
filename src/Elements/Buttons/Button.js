0fd3112ab1e37c67e0762304d69ce8618cd7460b
Wesley Chang <weschang15@gmail.com>
2019-07-06 00:27:37 -0700

    Updated styling to match new color scheme

--- src/Elements/Buttons/Button.js
+++ src/Elements/Buttons/Button.js
@@ -1,18 +1,30 @@
 import styled from "styled-components";
 
 const Button = styled.button`
-  font-size: 16px;
-  padding: 0 1.25em;
-  letter-spacing: 0.025em;
-  font-weight: 600;
-  height: 44px;
+  border-radius: 5px;
+  ${({ theme, withShadow }) => withShadow && theme.shadows.primary}
   cursor: pointer;
+  font-size: 1em;
+  font-weight: bold;
+  letter-spacing: 0.025em;
   margin: 0.5em 0;
+  padding: 0.5em 1.25em;
+  text-transform: uppercase;
+  transition: ${({ theme }) =>
+    `${theme.transitions.speed.fast} background-color ${
+      theme.transitions.easing.default
+    }, ${theme.transitions.speed.fast} border-color ${
+      theme.transitions.easing.default
+    }`};
   &:disabled {
-    border-color: ${({ theme }) => theme.button.disabled.bg};
-    background-color: ${({ theme }) => theme.button.disabled.bg};
-    color: ${({ theme }) => theme.button.disabled.color};
+    border-color: ${({ theme }) => theme.buttons.disabled.bg};
+    background-color: ${({ theme }) => theme.buttons.disabled.bg};
+    color: ${({ theme }) => theme.buttons.disabled.color};
   }
 `;
+
+Button.defaultProps = {
+  withShadow: true
+};
 
 export default Button;
