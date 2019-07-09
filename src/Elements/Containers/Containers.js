26f1553b706c0b13da86967f50986ba9e8e8b8b0
Wesley Chang <weschang15@gmail.com>
2019-07-08 21:54:38 -0700

    Added max width column sizes and center justification to create max width container

--- src/Elements/Containers/Containers.js
+++ src/Elements/Containers/Containers.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import PropTypes from "prop-types";
 
 export const PageContainer = styled.main`
   padding: 1em;
@@ -7,3 +8,17 @@ export const PageContainer = styled.main`
 export const SectionContainer = styled.div`
   margin: 35px 0;
 `;
+
+export const Wrapper = styled.div`
+  max-width: ${({ maxContainerWidth }) => `${maxContainerWidth}px`};
+  ${({ centered }) => centered && `margin: 0 auto`};
+`;
+
+Wrapper.propTypes = {
+  centered: PropTypes.bool,
+  maxContainerWidth: PropTypes.number.isRequired
+};
+
+Wrapper.defaultProps = {
+  maxContainerWidth: 1440
+};
